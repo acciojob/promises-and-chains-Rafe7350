3aff286f6c95fb7ed3cc89dcdc1c6874581f6d24
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-04-30 18:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// your JS code here. If required.
+// Get references to the form elements
 const nameInput = document.getElementById('name');
 const ageInput = document.getElementById('age');
 const submitBtn = document.getElementById('btn');
 
+// Add a listener to the form submission event
 submitBtn.addEventListener('click', (event) => {
-  event.preventDefault(); 
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Check if the inputs are valid
+  if (!nameInput.value || !ageInput.value) {
+    alert('Please enter valid details');
+    return;
+  }
+
+  // Create a promise that resolves after 4 seconds with an object
   const delayPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (Number(ageInput.value) >= 18) {
@@ -15,6 +24,7 @@ submitBtn.addEventListener('click', (event) => {
     }, 4000);
   });
 
+  // Handle the result of the promise
   delayPromise
     .then((data) => {
       alert(`Welcome, ${data.name}. You can vote.`);
